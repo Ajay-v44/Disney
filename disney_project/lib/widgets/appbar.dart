83d3f0d25242d0208b40b364932fa975233feebe
Ajay v <vajay4834@gmail.com>
2023-07-31 21:43:43 +0530

    bottom navigatiobar added

--- disney_project/lib/widgets/appbar.dart
+++ disney_project/lib/widgets/appbar.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-const disneylogo='assets/images/logo.jpeg';
-const kToolbarHeight=56.0;
+const disneyLogo = 'assets/images/disney-logo.jpeg';
+const kToolbarHeight = 56.0;
 
-AppBar disneyCustomAppBar(){
+AppBar disneyCustomAppBar() {
   return AppBar(
     backgroundColor: Colors.transparent,
     elevation: 0,
     centerTitle: true,
     title: SizedBox(
       height: kToolbarHeight,
-      child: Image.asset(disneylogo,fit:BoxFit.contain,),
+      child: Image.asset(
+        disneyLogo,
+        fit: BoxFit.contain,
+      ),
     ),
     toolbarHeight: kToolbarHeight,
   );
