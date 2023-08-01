e2dbd907410cbd13a727483b18d449aa6a34eac8
Ajay v <vajay4834@gmail.com>
2023-08-01 22:35:37 +0530

    added carousal to home screen

--- disney_project/lib/home.dart
+++ disney_project/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:disney_project/widgets/appbar.dart';
+import 'package:disney_project/widgets/carousal.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
