1a8fb4d5ab62dd2c3b0a1dee0a8a1f26c9d77927
Ajay v <vajay4834@gmail.com>
2023-08-01 22:44:40 +0530

    completed home page

--- disney_project/lib/home.dart
+++ disney_project/lib/home.dart
@@ -4,6 +4,7 @@ import 'package:disney_project/widgets/carousal.dart';
 import 'package:disney_project/widgets/bottomNavigationBar.dart';
 import 'package:disney_project/widgets/listMovies.dart';
 import 'package:disney_project/widgets/recomendedMovies.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,8 +13,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   static const backgroundColor = Color.fromARGB(30, 34, 49, 255);
-   static List<String> covers = [
+
+    static List<String> covers = [
     'https://media.comicbook.com/uploads1/2015/06/antmanposter-139745.jpg',
     'https://i.pinimg.com/236x/92/5d/e8/925de870f00d0f1f83502772bbc6c84c.jpg',
     'https://hips.hearstapps.com/digitalspyuk.cdnds.net/13/18/comics-infinity-1-cover-artwork.jpg',
@@ -26,8 +29,19 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: disneyCustomAppBar(),
-
-
+      body: ListView(
+        addAutomaticKeepAlives: true,
+        shrinkWrap: true,
+        children: [
+          SizedBox(height: 10,),
+          CustomCarousal(),
+          SizedBox(height: 10,),
+          ListMovies(),
+          SizedBox(height: 10,),
+          RecommendedMovies(covers: covers,)
+        ],
+      ),
+      bottomNavigationBar: bottomNavigationBar(),
     );
   }
 }
